1381036dcb1ad53d469e6f9b98e588f9a1073e28
rakeshghumatkar07 <rakeshghumatkar100@gmail.com>
2024-05-16 12:09:29 +0530

    Basic Controller

--- API/Program.cs
+++ API/Program.cs
@@ -24,4 +24,18 @@ if (app.Environment.IsDevelopment())
 
 app.MapControllers();
 
+//Addiding SQL lite database 
+using var scope = app.Services.CreateScope();
+var service = scope.ServiceProvider;
+try 
+{
+    var context = service.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedData(context);
+}
+catch (Exception ex)
+{
+    var logger = service.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex,"Error occure while migration");
+}
 app.Run();
